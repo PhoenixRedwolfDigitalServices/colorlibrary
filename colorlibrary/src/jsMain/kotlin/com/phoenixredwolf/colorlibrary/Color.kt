1138ffa3cde72d9977542b7e10d83d6b752ad150
phoenixredwolf <b.vogtman@outlook.com>
2023-09-27 16:21:09 -0400

    update readme

--- colorlibrary/src/jsMain/kotlin/com/phoenixredwolf/colorlibrary/Color.kt
+++ colorlibrary/src/jsMain/kotlin/com/phoenixredwolf/colorlibrary/Color.kt
@@ -11,5 +11,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+/**
+ * Add your own colors here
+ */
+
 val outerColor = com.varabyte.kobweb.compose.ui.graphics.Color
 fun Color.toCSS(): CSSColorValue = com.phoenixredwolf.colorlibrary.outerColor.argb(this.alpha, this.red, this.green, this.blue)
