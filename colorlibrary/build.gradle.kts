08adbb0aa77c2d1a01f9adcef2d383b7dbb340e5
phoenixredwolf <b.vogtman@outlook.com>
2023-09-27 13:26:29 -0400

    added pom file

--- colorlibrary/build.gradle.kts
+++ colorlibrary/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.phoenixredwolfdigitalservices.colorlibrary"
-version = "0.0.1"
+version = "1.0.0"
 
 kotlin {
     configAsKobwebLibrary(includeServer = false)
@@ -37,7 +37,7 @@ publishing {
             from(components["kotlin"])
             groupId = "com.github.phoenixredwolfdigitalservices"
             artifactId = "ColorLibrary"
-            version = "0.0.1"
+            version = "1.0.0"
         }
     }
 }
